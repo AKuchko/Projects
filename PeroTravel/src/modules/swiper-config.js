8df3b33056cb52508bb09ae07780dc7a966f0232
AK <101558236+AKuchko@users.noreply.github.com>
2022-10-20 21:39:33 +0700

    Pop. excursions & About sections

--- PeroTravel/src/modules/swiper-config.js
+++ PeroTravel/src/modules/swiper-config.js
@@ -1,23 +1,41 @@
-import Swiper from "swiper";
+import Swiper, { Navigation, Scrollbar } from "swiper";
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
 
-const breakpoints = {
-    768: {
-        slidesOffsetBefore: 289,
-        slidesOffsetAfter: 79,
-    },
-    1440: {
-        slidesOffsetBefore: 825,
-        slidesOffsetAfter: 160,
-    }
-}
+Swiper.use(Navigation);
+Swiper.use(Scrollbar);
+
+const spaceBetweenSlides = 20;
+const navigationSettings = { nextEl: '.base-slider__button-next', prevEl: '.base-slider__button-prev' };
+const scrollbarSettings = { el: '.base-slider__scrollbar' };
 
 const header_section_slider = new Swiper(
     '.header-section__slider', {
         slidesPerView: 'auto',
-        spaceBetween: 20,
+        spaceBetween: spaceBetweenSlides,
         slidesOffsetBefore: 24,
         slidesOffsetAfter: 24,
-        breakpoints: breakpoints,
+        breakpoints: {
+            768: {
+                slidesOffsetBefore: 289,
+                slidesOffsetAfter: 79,
+            },
+            1440: {
+                slidesOffsetBefore: 825,
+                slidesOffsetAfter: 160,
+            }
+        },
+    }
+)
+
+const popular_excursions = new Swiper(
+    '.popular-excursions__slider', 
+    {
+        slidesPerView: 'auto',
+        spaceBetween: spaceBetweenSlides,
+        slidesOffsetAfter: 24,
+        navigation: navigationSettings,
+        scrollbar: scrollbarSettings,
     }
 )
--- PeroTravel/webpack.config.js
+++ PeroTravel/webpack.config.js
@@ -93,7 +93,7 @@ module.exports = {
                 type: 'asset/resource',
             },
             {
-                test: /\.(otf|waff2?)$/i,
+                test: /\.(otf|waff2?|ttf)$/i,
                 type: 'asset/resource',
                 generator: {
                     filename: 'fonts/[name][ext]'
